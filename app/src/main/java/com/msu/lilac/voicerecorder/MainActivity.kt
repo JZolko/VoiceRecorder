0a2775a45a164725adcfe30218215a8a5d6b373b
Jake Zolkosky <zolkosky@cse.msu.edu>
2022-09-16 00:48:21 -0400

    mod

--- app/src/main/java/com/msu/lilac/voicerecorder/MainActivity.kt
+++ app/src/main/java/com/msu/lilac/voicerecorder/MainActivity.kt
@@ -1,49 +1,46 @@
 package com.msu.lilac.voicerecorder
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.icu.util.Calendar
-import android.media.AudioFormat
-import android.media.AudioRecord
-import android.media.MediaRecorder
+
 import android.os.Bundle
-import android.os.Environment
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private val _soundRecorder = SoundRecorder(this)
     private var _isRecording = false
+    private var _isPlaying = false
+    val recordButton: Button by lazy { findViewById<Button>(R.id.RecordButton) }
+    val stopButton: Button by lazy { findViewById<Button>(R.id.StopButton) }
+    val playButton: Button by lazy { findViewById<Button>(R.id.PlayButton) }
+    val pauseButton: Button by lazy { findViewById<Button>(R.id.PauseButton) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
 
         //login view
 
         // other login view
 
         // main audio record view
-        val recordButton = findViewById<Button>(R.id.RecordButton)
         recordButton.setOnClickListener {
-            if (!_isRecording) {
-                _soundRecorder.startRecording()
-                _isRecording = true
-                recordButton.text = "Recording"
-            }
+            _soundRecorder.startRecording()
         }
 
-        val stopButton = findViewById<Button>(R.id.StopButton)
         stopButton.setOnClickListener {
             _soundRecorder.stopRecording()
-            _isRecording = false
-            stopButton.text = "Stopped"
-            recordButton.text = "Record"
+        }
+
+        pauseButton.setOnClickListener {
+            _soundRecorder.pausePlayback()
+        }
+
+        playButton.setOnClickListener {
+            _soundRecorder.playPlayback()
         }
     }
 }
--- app/src/main/java/com/msu/lilac/voicerecorder/SoundRecorder.kt
+++ app/src/main/java/com/msu/lilac/voicerecorder/SoundRecorder.kt
@@ -2,28 +2,51 @@ package com.msu.lilac.voicerecorder
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.icu.util.Calendar
-import android.media.AudioFormat
 import android.media.AudioRecord
+import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
-import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlin.math.log
 
 class SoundRecorder(mainActivity: MainActivity) {
 
     private var _mainActivity: MainActivity = mainActivity
     private var _isRecording = false
     private var _mediaRecorder: MediaRecorder? = null
+    private var _mediaPlayer = MediaPlayer()
     private var _currentFile = 0
-    private var _fileName =Environment.getExternalStorageDirectory().getAbsolutePath() +  "/AudioRecording.3gp"
+    private var _fileName = Environment.getExternalStoragePublicDirectory(
+        Environment.DIRECTORY_DOWNLOADS
+    ).toString() + "/AudioRecording.3gp"
+    //private var _fileName = "/sdcard/Download/AudioRecording.3gp"
+    private var _isPlaying = false
+
 
     private fun CheckPermissions() {
+        val permissionRecord = ContextCompat.checkSelfPermission(_mainActivity,
+            Manifest.permission.RECORD_AUDIO)
+        val permissionWriteStorage = ContextCompat.checkSelfPermission(_mainActivity,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val permissionReadStorage = ContextCompat.checkSelfPermission(_mainActivity,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        val permissionReadInternalStorage = ContextCompat.checkSelfPermission(_mainActivity,
+            Manifest.permission.MANAGE_MEDIA)
+
+        if (permissionRecord != PackageManager.PERMISSION_GRANTED ||
+            permissionWriteStorage != PackageManager.PERMISSION_GRANTED ||
+            permissionReadStorage != PackageManager.PERMISSION_GRANTED ||
+            permissionReadInternalStorage != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(_mainActivity,
+                arrayOf(Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        }
+        _mediaPlayer = MediaPlayer()
+        _mediaRecorder = MediaRecorder()
 
     }
     fun startRecording() {
@@ -38,7 +61,8 @@ class SoundRecorder(mainActivity: MainActivity) {
                 Manifest.permission.RECORD_AUDIO
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            _mediaRecorder?.setAudioEncodingBitRate(16)
+            _mediaRecorder?.setAudioEncodingBitRate(96)
+            _mediaRecorder?.setAudioSamplingRate(48000)
             _mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
             _mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
             _mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
@@ -47,9 +71,12 @@ class SoundRecorder(mainActivity: MainActivity) {
                 _mediaRecorder?.prepare()
                 _mediaRecorder?.start()
                 _isRecording = true
+                _mainActivity.recordButton.text = "Recording"
             } catch (e: Exception) {
                 e.printStackTrace()
                 Log.e("TAG", "prepare() failed" + e)
+                _mainActivity.recordButton.text = "failed"
+
             }
         }
     }
@@ -58,10 +85,47 @@ class SoundRecorder(mainActivity: MainActivity) {
         if(!_isRecording){
             return
         }
-        _mediaRecorder?.stop()
-        _mediaRecorder?.release()
-        _isRecording = false
-        _currentFile++
+        try {
+            _mediaRecorder?.stop()
+            _mediaRecorder?.release()
+            _isRecording = false
+            _currentFile++
+            _mainActivity.recordButton.text = "NO"
+            _mainActivity.stopButton.text = "Stopped"
+        }catch (e: Exception){
+            e.printStackTrace()
+            Log.e("TAG", "stop prepare() failed" + e)
+            _mainActivity.stopButton.text = "failed"
+        }
 
+    }
+
+    fun pausePlayback() {
+        if(_isRecording || !_isPlaying) {
+            return
+        }
+        _mediaPlayer.pause()
+        _isRecording = false
+        _isPlaying = false
+    }
+
+    fun playPlayback() {
+        if (_isRecording) {
+            return
+        }
+
+        try {
+            _mediaPlayer.setDataSource(_fileName)
+            _mediaPlayer.prepare()
+            _mediaPlayer.start()
+            _isRecording = false
+            _isPlaying = true
+            _mainActivity.playButton.text = "Playing"
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("TAG", "prepare() failed" + e)
+            _mainActivity.playButton.text = "failed"
+
+        }
     }
 }
